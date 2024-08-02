fce24224c332ba0b001ef15544b9c088c6e832f3
hirakRajbonshi <hrkraj0@gmail.com>
2024-08-03 01:24:50 +0530

    added authentication

---
+++ src/app/userProvider.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+import { User, onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/lib/firebase/firbaseConfig";
+
+const UserContext = createContext<User | null>(null);
+
+export function UserProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+      setUser(authUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+}
+
+export function useUser() {
+  return useContext(UserContext);
+}
